8289dbf79dc9aadbf5daacba79e976e488836b83
Grace Mae <averymatherne@gmail.com>
2026-04-15 16:07:23 -0500

    Pull number blocks up to offset font metric whitespace on values heading

--- src/app/about/AboutPageContent.tsx
+++ src/app/about/AboutPageContent.tsx
@@ -193,7 +193,7 @@ export default function AboutPageContent({ site, about, values }: Props) {
             </RevealText>
           </motion.div>
 
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-10 lg:gap-14 mt-8">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-10 lg:gap-14 -mt-6">
             {values.map((value, i) => (
               <motion.div
                 key={value._id}
